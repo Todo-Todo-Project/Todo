ef8e0a230f167432e2019781434788d5b22dab3f
irisdecembre <thanhtrang181220@gmail.com>
2022-03-30 21:26:51 +0700

    Trang | Update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import {Container, Navbar, ThemeProvider, Row, Col} from 'react-bootstrap';
-import TodoList from '../TodoList/TodoList';
+import { Col, Container, Navbar, Row, ThemeProvider } from 'react-bootstrap';
+import ToDoList from '../TodoList';
+
 function App() {
-  const todos = [{id: 1, name:'learn english', isCompleted: true}, {id: 2, name:'learn spa', isCompleted: false}]
+  const todos = [{id: 1, name: 'Learn English', isCompleted: true}, {id: 2, name: 'Learn Spanish', isCompleted: false}];
   return (
-    <ThemeProvider breakpoints={['xxl', 'xl', 'lg', 'md', 'sm', 'xs']}>
-      <Navbar bg='light'>
+    <ThemeProvider
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+    >
+      <Navbar bg="light">
         <Container>
-          <Navbar.Brand href="home">Brank Link</Navbar.Brand>
+          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
         </Container>
       </Navbar>
       <Container>
         <Row>
-          <Col md={4}>Left Menu</Col>
-          <Col md={8}><TodoList todos={todos}/></Col>
+          <Col md={4}>Left menu</Col>
+          <Col md={8}><ToDoList todos={todos} /></Col>
         </Row>
       </Container>
     </ThemeProvider>
