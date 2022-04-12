33a73c47a3b7ad40bf1d1e843cf547b234c6f146
irisdecembre <thanhtrang181220@gmail.com>
2022-04-13 01:58:33 +0700

    Trang | Fix typo

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Col, Container, Navbar, Row, ThemeProvider } from 'react-bootstrap';
-import ToDoList from '../TodoItem';
+import TodoItem from '../TodoItem';
 
 function App() {
   const todos = [{id: 1, name: 'Learn English', isCompleted: true}, {id: 2, name: 'Learn Spanish', isCompleted: false}];
@@ -16,7 +16,7 @@ function App() {
       <Container>
         <Row>
           <Col md={4}>Left menu</Col>
-          <Col md={8}><ToDoList todos={todos} /></Col>
+          <Col md={8}><TodoItem todos={todos} /></Col>
         </Row>
       </Container>
     </ThemeProvider>
