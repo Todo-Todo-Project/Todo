c60a5fb89a73d64cb1f2a0153b8aed58135b90ba
irisdecembre <thanhtrang181220@gmail.com>
2022-04-14 12:41:35 +0700

    Trang | Update UI

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -10,18 +10,19 @@ function App() {
     <ThemeProvider
       breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
     >
-      <Navbar bg="light">
+      <Navbar className="navbar">
         <Container>
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <div class="todo-title">Todo Todo</div></Navbar.Brand>
         </Container>
       </Navbar>
       <Container>
-        <Row>
-          <Col md={4}>Left menu</Col>
-
-          <Col md={8}><TodoList todos={todos} /></Col>
-
-        </Row>
+        {/* <Col md={1}>
+          <Row md={1}>Left menu</Row>
+        </Col> */}
+        <Col md={2}>
+          <Row md={12}><TodoList todos={todos} /></Row>
+        </Col>
       </Container>
     </ThemeProvider>
   );
