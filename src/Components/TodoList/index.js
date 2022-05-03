33dc493ca9c81c7f6a923c319f55dfe3b669673c
BerNardTran <tbkhanh0908@gmail.com>
2022-05-03 19:33:01 +0700

    add sort function

--- src/Components/TodoList/index.js
+++ src/Components/TodoList/index.js
@@ -1,10 +1,10 @@
 import "./TodoList.css";
 import TodoItem from "../TodoItem";
-import AddTodo from "./../AddTodo/addTodo"
+import AddTodo from "./../AddTodo/addTodo";
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlinePlus } from "react-icons/ai";
 import { BsFillTrashFill } from "react-icons/bs";
 import axios from "axios";
 
@@ -12,16 +12,16 @@ function TodoList() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [todos, setTodos] = useState([]);
-    const [listDelete, setListDelete] = useState([])
+    const [listDelete, setListDelete] = useState([]);
     const navigate = useNavigate();
 
     // Note: the empty deps array [] means
     // this useEffect will run once
     // similar to componentDidMount()
     useEffect(() => {
-      if (!isLoaded) {
-        loadAllTodos();
-      }
+        if (!isLoaded) {
+            loadAllTodos();
+        }
     }, [isLoaded]);
 
     function loadAllTodos() {
@@ -87,14 +87,14 @@ function TodoList() {
     }
 
     function deleteListTodo() {
-        for (let i =0; i< listDelete.length; i++) {
+        for (let i = 0; i < listDelete.length; i++) {
             axios
-            .delete("http://localhost:3000/todos/" + listDelete[i])
-            .then((res) => console.log(res));
+                .delete("http://localhost:3000/todos/" + listDelete[i])
+                .then((res) => console.log(res));
         }
         window.location.reload();
     }
-
+    // sortDeByName(todos);
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -102,15 +102,26 @@ function TodoList() {
     } else {
         return (
             <div>
-              <div className="todolist_addTodo" onClick={()=> console.log("ddddd")}>
-                Add new Todo
-               <AddTodo></AddTodo>
-               <BsFillTrashFill onClick={()=>deleteListTodo() }></BsFillTrashFill>
-              </div>
+                <button onClick={()=> {
+                    setTodos([...sortByPriority(todos,true)])
+                }}>
+                    button demo sort
+                </button>
+                <div
+                    className="todolist_addTodo"
+                    onClick={() => console.log("ddddd")}
+                >
+                    Add new Todo
+                    <AddTodo></AddTodo>
+                    <BsFillTrashFill
+                        onClick={() => deleteListTodo()}
+                    ></BsFillTrashFill>
+                </div>
                 <ul className="todo-list">
                     {todos.map((todo) => (
                         <TodoItem
-                        callBackWhenCheckedIsFalse={callBackCheckedIsFalse} callBackWhenCheckedIsTrue={callBackCheckedIsTrue}
+                            callBackWhenCheckedIsFalse={callBackCheckedIsFalse}
+                            callBackWhenCheckedIsTrue={callBackCheckedIsTrue}
                             key={todo._id}
                             name={todo.name}
                             isCompleted={todo.isCompleted}
@@ -124,15 +135,100 @@ function TodoList() {
     }
 
     function callBackCheckedIsFalse(id) {
-        let index = listDelete.indexOf(id)
+        let index = listDelete.indexOf(id);
         if (index !== -1) {
-            listDelete.splice(index,1)
+            listDelete.splice(index, 1);
         }
     }
 
     function callBackCheckedIsTrue(id) {
-        listDelete.push(id)
+        listDelete.push(id);
     }
 }
 
 export default TodoList;
+
+function sortByPriority(arr, isIn) {
+    let lowArry = [];
+    let normalArray = [];
+    let hightArry = [];
+    let ugentArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        if (arr[i].priority === "Low") {
+            lowArry.push(arr[i]);
+        }
+        if (arr[i].priority === "Normal") {
+            normalArray.push(arr[i]);
+        }
+        if (arr[i].priority === "High") {
+            hightArry.push(arr[i]);
+        }
+        if (arr[i].priority === "Urgent") {
+            ugentArray.push(arr[i]);
+        }
+    }
+
+    if (isIn === true) {
+        const sortedList = lowArry
+            .concat(normalArray)
+            .concat(hightArry)
+            .concat(ugentArray);
+        return sortedList;
+    }
+
+    if (isIn === false) {
+        const sortedList = ugentArray
+            .concat(hightArry)
+            .concat(normalArray)
+            .concat(lowArry);
+        return sortedList;
+    }
+}
+//==========================================================
+function sortDeByCreationDate(arr) {
+    const sorted = arr.sort((a, b) => {
+        return new Date(b.creationdate) - new Date(a.creationdate);
+    });
+    return sorted;
+}
+
+function sortInByCreationDate(arr) {
+    const sorted = arr.sort((a, b) => {
+        return new Date(a.creationdate) - new Date(b.creationdate);
+    });
+    return sorted;
+}
+
+function sortDeByDueDate(arr) {
+    const sorted = arr.sort((a, b) => {
+        return new Date(b.duedate) - new Date(a.duedate);
+    });
+    return sorted;
+}
+
+function sortInByDueDate(arr) {
+    const sorted = arr.sort((a, b) => {
+        return new Date(a.duedate) - new Date(b.duedate);
+    });
+    return sorted;
+}
+//===========================================================
+
+function sortInByName(arr) {
+    let sorted = arr.sort((a, b) => 
+        a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+    );
+    console.log(sorted)
+    return sorted;
+}
+
+function sortDeByName(arr) {
+    let sorted = arr.sort((b, a) => 
+        a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+    );
+    console.log(sorted)
+    return sorted;
+}
+
+//=========================================================
